d87c15880ba0384085520c48d81e0a73d114feca
BoykinChas <115953792+BoykinChas@users.noreply.github.com>
2023-02-12 17:45:23 -0600

    file updates

--- capstone/capstone/settings.py
+++ capstone/capstone/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'djf_surveys',
     'meddata',
     'users',
     
@@ -120,7 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
 
 
 # Default primary key field type
